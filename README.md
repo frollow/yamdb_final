be62ef6bc22f9102d14079f06b2885fad2b69d97
Artem Frolov <Artem.Frolov@kaspersky.com>
2022-02-17 11:35:27 +0300

    feat(ya): project added

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# yamdb_final
-yamdb_final
+
+# api_yamdb
+
+![example workflow](https://github.com/frollow/yamdb_final/actions/workflows/yamdb_workflow/badge.svg)
+
+REST API для сервиса YaMDb — базы отзывов о фильмах, книгах и музыке. (Коллективный проект 3 студентов Яндекс.Практикум)
+
+## Описание
+API для сервиса YaMDb.
+Позволяет работать со следующими сущностями:
+- Отзывы
+- Коментарии к отзывам
+- Пользователи
+- Категории (типы) произведений
+- Категории жанров
+- Произведения (на которые пишут отзывы)
+
+ ##  Шаблон .env файла
+Создайте файл в этой директории:
+
+    /infra_sp2/infra/...
+
+ Заполните его, и придумайте логин и пароль пользователя для базы данных, 
+ вместо postgres укажите свои значения.
+
+    DB_ENGINE=django.db.backends.postgresql
+    DB_NAME=postgres
+    POSTGRES_USER=postgres
+    POSTGRES_PASSWORD=postgres
+    DB_HOST=db
+    DB_PORT=5432
+
+## Запуск программы
+
+Выполните команду для запуска контейнеров:
+
+    docker-compose -f infra/docker-compose.yaml up -d --build
+
+Сделайте миграции:
+
+    docker-compose exec web python manage.py makemigrations
+    docker-compose exec web python manage.py migrate
+
+Создайте пользователя:
+
+    docker-compose exec web python manage.py createsuperuser
+
+## Описание Api методов
+http://localhost/redoc/
+
+### Автор
+Артем Фролов
---
+++ api_yamdb/api/views/titles.py
@@ -0,0 +1,43 @@
+from api.filters import TitleFilters
+from api.serializers import (CategorySerializer, GenreSerializer,
+                             TitleSerializer, TitleWriteSerializer)
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, mixins, viewsets
+from titles.models import Category, Genre, Title
+from users.permissions import IsAdminOrReadOnly
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    lookup_field = 'slug'
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+    permission_classes = (IsAdminOrReadOnly,)
+
+
+class GenreViewSet(
+    mixins.CreateModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    lookup_field = 'slug'
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+    permission_classes = (IsAdminOrReadOnly,)
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    filter_backends = (DjangoFilterBackend, TitleFilters)
+    filterset_fields = ('year',)
+    permission_classes = (IsAdminOrReadOnly,)
+
+    def get_serializer_class(self):
+        if self.action in ('list', 'retrieve'):
+            return TitleSerializer
+
+        return TitleWriteSerializer
